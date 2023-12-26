fba5031d0aefffff751e6634d37c88b82daccf69
Daniel <dnl.canedo@gmail.com>
2023-12-26 11:26:30 -0400

    WIP

---
+++ RecorderAndPlayer/Recorder.swift
@@ -0,0 +1,8 @@
+//
+//  Recorder.swift
+//  RecorderAndPlayer
+//
+//  Created by Daniel on 2/8/22.
+//
+
+import Foundation
--- RecorderAndPlayer/ViewController.swift
+++ RecorderAndPlayer/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onRecordTap(_ sender: Any) {
+        print("Record!")
+    }
 }
 
